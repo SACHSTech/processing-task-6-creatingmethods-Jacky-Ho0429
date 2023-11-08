93b1fd622268a4a561612eb6daf9569ef06d3a20
Jacky-Ho0429 <jacky.ho25@ycdsbk12.ca>
2023-11-08 17:51:19 -0500

    test

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -23,14 +23,43 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    drawHouse(25, 290, 255,255,255);
+    drawHouse(40, 290, 255, 255,255);
   }
   
-  // define other methods down here.
+  /**
+   * 
+   * @param x1 Base top left corner x-coordinate
+   * @param y1 Base top left corner x-coordinate
+   * @param red Percentage of red color for fill
+   * @param green Percentage of green color for fill
+   * @param blue Percentage of blue color for fill
+   */
+  public void drawHouse(int x1, int y1, int red, int green, int blue) {
+
+    // Base
+    stroke(0, 0, 0);
+    fill(240, 250, 190);
+    rect(x1, y1, 120, 100);
+
+    // Roof
+    stroke(red, green, blue);
+    fill(210, 100, 75);
+    triangle(0, 290, 170, 290, 85, 215);
+  
+    // Window
+    stroke(255, 255, 255);
+    fill(140, 220, 240);
+    rect(50, 325, 30, 30);
+    line(65, 325, 65, 355);
+    line(50, 340, 80, 340);
+  
+    // Door
+    stroke(0, 0, 0);
+    fill(170, 105, 50);
+    rect(97, 340, 25, 50);
+    stroke(255, 140, 105);
+    fill(255, 140, 105);
+    ellipse(115, 365, 3, 3);
+  }
 }
